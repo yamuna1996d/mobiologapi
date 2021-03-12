b062623811285eb7cc3e4427670b3c46fda2d3b4
yamuna1996d <46672811+yamuna1996d@users.noreply.github.com>
2021-03-12 15:26:17 +0530

    login

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const parser = require("body-parser");
+const app =express();
+const Register = require("./models/UserSchema");
+
+// app.use(parser.urlencoded({extended:false}));
+app.use(express.json());
+mongoose.connect("mongodb+srv://usserdb123:usserdb123@cluster0.opis6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("database connection successful");
+})
+.catch((err) => {
+    console.log("database connection error " + err);
+});
+
+app.get("/",(req,res)=>{
+    res.send("Hai");
+});
+
+app.post("/register",async (req,res)=>{
+    try {
+            const reg = new Register(req.body);
+            const registed =await reg.save();
+            console.log(registed);
+            // res.status(201).render("index");
+        
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+app.post("/login",async(req,res)=>{
+    try {
+        var searchuser = req.body.usern;
+        var searchpassword = req.body.pass;
+        Register.find({
+            $and: [
+                {
+                    "email": searchuser
+                },
+                {
+                    "password": searchpassword
+                }
+            ]
+        }, (error, data) => {
+            if (error) {
+                throw error;
+            }
+            if (data.length > 0) {
+                res.json({ "status": "Success" });
+            }
+            else {
+                res.json({ "status": "Failed" });
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("server started");
+});
---
+++ models/UserSchema.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    userName: { type: String, required: true,trim:true },
+    email: { type: String, required: true, unique: true,trim:true },
+    password: { type: String, required: true,trim:true},
+    conpassword:{ type: String, required: true,trim:true },
+}, { timestamps: true });
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
