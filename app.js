87d47de8a1163644350d728468d6abfeb3ec47a4
yamuna1996d <46672811+yamuna1996d@users.noreply.github.com>
2021-03-12 16:51:58 +0530

    login

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const parser = require("body-parser");
+const parser = require("body-parser");
 const app =express();
 const Register = require("./models/UserSchema");
 
-// app.use(parser.urlencoded({extended:false}));
-app.use(express.json());
+app.use(parser.urlencoded({extended:false}));
+//app.use(express.json());
 mongoose.connect("mongodb+srv://usserdb123:usserdb123@cluster0.opis6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
     useNewUrlParser: true,
     useCreateIndex: true,
