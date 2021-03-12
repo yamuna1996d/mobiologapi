172c13334c4637139f27e720c6f80e48e89776b0
yamuna1996d <46672811+yamuna1996d@users.noreply.github.com>
2021-03-12 18:27:26 +0530

    login

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.post("/login",async(req,res)=>{
                 throw error;
             }
             if (data.length > 0) {
-                res.render("Welcome");
+                res.status(201).render("Welcome");
             }
             else {
                 res.json({ "status": "Failed" });
