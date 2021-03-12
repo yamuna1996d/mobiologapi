62362d2dfd6cc5dd932d1d1a59599d5c959ac667
yamuna1996d <46672811+yamuna1996d@users.noreply.github.com>
2021-03-12 22:27:21 +0530

    login

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post("/register",async (req,res)=>{
              
         
     } catch (error) {
-        res.status(400).send("<h1>Values Are Not Stored</h1>error");
+        res.status(400).send("<h1>Values Are Not Stored Because email is already stored earlier!!</h1>");
     }
 });
 
